c66d86d71a890883e3c54254fc79338f8ef40552
twride17 <toby@thewrides.com>
2022-11-12 16:32:50 +0000

    Unit testing of MethodArgumentRule

--- src/main/java/tw/jruletest/parse/rules/MethodArgumentRule.java
+++ src/main/java/tw/jruletest/parse/rules/MethodArgumentRule.java
@@ -7,18 +7,7 @@ public class MethodArgumentRule implements Rule {
 
     @Override
     public String decodeRule(String rule) {
-        rule = rule.replace("with arguments", "").replace(":", "");
-        ArrayList<String> terms = new ArrayList<>(Arrays.asList(rule.split(" ")));
-
-        boolean andRemaining;
-        do {
-            andRemaining = terms.remove("and");
-        } while(andRemaining);
-
-        String argList = terms.get(0).replace(",", "");
-        for(int i = 1; i < terms.size(); i++) {
-            argList += ", " + terms.get(i);
-        }
-        return argList;
+        rule = rule.replace("with arguments", "").replace(":", "").trim();
+        return rule.replace(" and", ",");
     }
 }
