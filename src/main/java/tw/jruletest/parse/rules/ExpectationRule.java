586d16a13ab5c51d3d93b70ea07f0e97c79d656c
twride17 <toby@thewrides.com>
2022-12-27 21:58:09 +0000

    Expectation rule properly parses rule content to form expectation code

--- src/main/java/tw/jruletest/parse/rules/ExpectationRule.java
+++ src/main/java/tw/jruletest/parse/rules/ExpectationRule.java
@@ -3,10 +3,14 @@ package tw.jruletest.parse.rules;
 import tw.jruletest.analyzers.ImportCollector;
 
 public class ExpectationRule implements Rule {
+
     @Override
     public String decodeRule(String rule) {
+        String[] parts = rule.trim().split(" ");
         ImportCollector.addImport("import tw.jruletest.expectations.*;");
-        return "Expectations.expect(value)." +
-                "toEqual(13);";
+        String code = "Expectations.expect(" + parts[0] + ").to";
+        String firstChar = String.valueOf(parts[2].charAt(0)).toUpperCase();
+        parts[2] = firstChar + parts[2].substring(1);
+        return code + parts[2] + "(" + parts[3] + ");";
     }
 }
