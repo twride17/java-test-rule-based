4c6af86ff86f52902f5597237d0d092617acc16f
twride17 <toby@thewrides.com>
2023-02-21 14:19:48 +0000

    Fixed testing of example parsing

--- src/test/java/tw/jruletest/parse/TestParserOnExamples.java
+++ src/test/java/tw/jruletest/parse/TestParserOnExamples.java
@@ -9,24 +9,19 @@ import java.nio.file.Paths;
 
 public class TestParserOnExamples {
 
-    private final String[][] RULES = {{"Call method Test.setValue with arguments: 6", "Get value of Test.x",
-                                        "Call method Test.setValue with: 56","Get value of Test.x",
-                                        "Expect xValue2 to not equal xValue"}};
-    private final String[] EXPECTED_CODE = {"Test.setValue(6);\nint xValue = Test.x;\n" +
-                                            "Test.setValue(56);\nint xValue2 = Test.x;\n" +
-                                            "Expectations.expect(xValue2).toNotEqual(xValue);\n"};
-
     @Before
     public void setup() {
         FileFinder.collectFiles(System.getProperty("user.dir") + "\\src\\test\\java");
     }
 
-    // Test with the first example program
     @Test
     public void testParserExample1() {
         String rules = "Call method Example.exampleMethod with arguments: 25\nStore value of Example.example in x\n" +
                         "Call Test.setValue with: 56\nGet Test.x\nExpect xValue to not equal x\nCall Test.setValue with: -100\n" +
                         "Expect value of Test.x to equal -100";
+        String expectedCode = "Example.exampleMethod(25);\nint x = Example.example;\nTest.setValue(56);\nint xValue = Test.x;\n" +
+                                "Expectations.expect(xValue).toNotEqual(x);\nTest.setValue(-100);\nExpectations.expect(Test.x).toEqual(-100);\n";
+        Assert.assertEquals(expectedCode, Parser.parseRules(rules.split("\n")));
     }
 
     @After
