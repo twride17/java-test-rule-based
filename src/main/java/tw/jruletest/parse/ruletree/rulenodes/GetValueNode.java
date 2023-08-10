a2522bc98b14c24d347ce0ee059c3a03f0ec5d8f
twride17 <toby@thewrides.com>
2023-08-10 20:25:39 +0100

    Previous Get Value node validation logic moved to ValueNode. Get Value node now adds surrounding code for generation

--- src/main/java/tw/jruletest/parse/ruletree/rulenodes/GetValueNode.java
+++ src/main/java/tw/jruletest/parse/ruletree/rulenodes/GetValueNode.java
@@ -2,60 +2,25 @@ package tw.jruletest.parse.ruletree.rulenodes;
 
 import tw.jruletest.exceptions.InvalidRuleStructureException;
 import tw.jruletest.parse.ruletree.TreeNode;
-import tw.jruletest.parse.ruletree.argumentnodes.VariableNode;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class GetValueNode implements TreeNode {
 
-    private TreeNode callTree;
+    private TreeNode valueNode;
 
     @Override
     public String generateCode() {
-        return callTree.generateCode();
+        // TODO derive valid variable name
+        // TODO derive required variable type
+        return "variable = " + valueNode.generateCode() + ";";
     }
 
     public int validateRule(String ruleContent) throws InvalidRuleStructureException {
-        Pattern regex = Pattern.compile("(((G|g)et\\s)?(value|result)\\sof\\s)?(.+)");
-        Matcher matcher = regex.matcher(ruleContent);
-
-        int currentEnd = 0;
-        String nextSegment = ruleContent;
-        if(matcher.find()) {
-            if(nextSegment.toLowerCase().startsWith("get ")) {
-                nextSegment = nextSegment.substring(4);
-                currentEnd += 4;
-            } else if(nextSegment.equals("get") || nextSegment.equals("Get")) {
-                throw new InvalidRuleStructureException(ruleContent, "Get Value Node");
-            }
-
-            if(nextSegment.startsWith("of ") || (nextSegment.equals("of"))) {
-                throw new InvalidRuleStructureException(ruleContent, "Get Value Node");
-            } else if(nextSegment.startsWith("value ") || nextSegment.startsWith("result ")) {
-                currentEnd += nextSegment.indexOf(' ') + 1;
-                nextSegment = nextSegment.substring(nextSegment.indexOf(' ') + 1);
-                if(!nextSegment.startsWith("of ")) {
-                    throw new InvalidRuleStructureException(ruleContent, "Get Value Node");
-                } else {
-                    currentEnd += nextSegment.indexOf(' ') + 1;
-                    nextSegment = nextSegment.substring(nextSegment.indexOf(' ') + 1);
-                }
-            } else if(nextSegment.equals("value") || nextSegment.equals("result")) {
-                throw new InvalidRuleStructureException(ruleContent, "Get Value Node");
+        if(ruleContent.split(" ")[0].equalsIgnoreCase("get")) {
+            if(ruleContent.length() > 4) {
+                valueNode = new ValueNode();
+                return 4 + valueNode.validateRule(ruleContent.substring(4));
             }
         }
-
-        callTree = new MethodNode();
-        try {
-            return callTree.validateRule(nextSegment) + currentEnd;
-        } catch(InvalidRuleStructureException e) {
-            try {
-                callTree = new VariableNode();
-                return currentEnd + callTree.validateRule(nextSegment);
-            } catch(InvalidRuleStructureException e2) {
-                throw new InvalidRuleStructureException(nextSegment, "Get Value Node");
-            }
-        }
+        throw new InvalidRuleStructureException(ruleContent, "Get Value Node");
     }
 }
---
+++ src/main/java/tw/jruletest/parse/ruletree/rulenodes/ValueNode.java
@@ -0,0 +1,54 @@
+package tw.jruletest.parse.ruletree.rulenodes;
+
+import tw.jruletest.exceptions.InvalidRuleStructureException;
+import tw.jruletest.parse.ruletree.TreeNode;
+import tw.jruletest.parse.ruletree.argumentnodes.VariableNode;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValueNode implements TreeNode {
+
+    private TreeNode valueSourceNode;
+
+    @Override
+    public String generateCode() {
+        return valueSourceNode.generateCode();
+    }
+
+    public int validateRule(String ruleContent) throws InvalidRuleStructureException {
+        Pattern regex = Pattern.compile("^((value|result)\\sof\\s)?(.+)");
+        Matcher matcher = regex.matcher(ruleContent);
+
+        int currentEnd = 0;
+        String nextSegment = ruleContent;
+        if(matcher.find()) {
+            if(nextSegment.startsWith("of ") || (nextSegment.equals("of"))) {
+                throw new InvalidRuleStructureException(ruleContent, "Value Node");
+            } else if(nextSegment.startsWith("value ") || nextSegment.startsWith("result ")) {
+                currentEnd += nextSegment.indexOf(' ') + 1;
+                nextSegment = nextSegment.substring(nextSegment.indexOf(' ') + 1);
+                if(!nextSegment.startsWith("of ")) {
+                    throw new InvalidRuleStructureException(ruleContent, "Value Node");
+                } else {
+                    currentEnd += nextSegment.indexOf(' ') + 1;
+                    nextSegment = nextSegment.substring(nextSegment.indexOf(' ') + 1);
+                }
+            } else if(nextSegment.equals("value") || nextSegment.equals("result")) {
+                throw new InvalidRuleStructureException(ruleContent, "Value Node");
+            }
+        }
+
+        valueSourceNode = new MethodNode();
+        try {
+            return valueSourceNode.validateRule(nextSegment) + currentEnd;
+        } catch(InvalidRuleStructureException e) {
+            try {
+                valueSourceNode = new VariableNode();
+                return currentEnd + valueSourceNode.validateRule(nextSegment);
+            } catch(InvalidRuleStructureException e2) {
+                throw new InvalidRuleStructureException(nextSegment, "Value Node");
+            }
+        }
+    }
+}
