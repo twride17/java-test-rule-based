82fe2d555c400f4765fbfb9ef0fac8da6c98bcbc
twride17 <toby@thewrides.com>
2023-08-13 14:13:11 +0100

    Variable store updated, uses new Variable class to easily get types of variables

---
+++ src/main/java/tw/jruletest/translation/Variable.java
@@ -0,0 +1,22 @@
+package tw.jruletest.translation;
+
+import java.lang.reflect.Type;
+
+public class Variable {
+
+    private String name;
+    private Type type;
+
+    public Variable(String name, Type type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+}
--- src/main/java/tw/jruletest/translation/VariableStore.java
+++ src/main/java/tw/jruletest/translation/VariableStore.java
@@ -1,28 +1,29 @@
 package tw.jruletest.translation;
 
+import java.lang.reflect.Type;
 import java.util.*;
 
 public class VariableStore {
 
-    private static Map<String, ArrayList<String>> variables = new HashMap<>();
+    private static Map<String, ArrayList<Variable>> variables = new HashMap<>();
 
-    public static void addVariable(String method, String varName) {
+    public static void addVariable(String method, String varName, Type varType) {
         try {
-            variables.get(method).add(varName);
+            variables.get(method).add(new Variable(varName, varType));
         } catch(NullPointerException e) {
-            variables.put(method, new ArrayList<>(Collections.singleton(varName)));
+            variables.put(method, new ArrayList<>(Collections.singleton(new Variable(varName, varType))));
         }
     }
 
-    public static String getNextUnusedVariableName(String method, String variableName) {
+    public static String getNextUnusedVariableName(String method, String variableName, Type type) {
         try {
-            ArrayList<String> methodVars = getVars(method);
+            ArrayList<String> methodVars = getVarNames(method);
             if(methodVars.contains(variableName)) {
                 variableName += countSimilar(methodVars, variableName);
             }
         } catch(NullPointerException e) { }
 
-        addVariable(method, variableName);
+        addVariable(method, variableName, type);
         return variableName;
     }
 
@@ -40,7 +41,21 @@ public class VariableStore {
         variables = new HashMap<>();
     }
 
-    public static ArrayList<String> getVars(String method) {
-        return variables.get(method);
+    public static ArrayList<String> getVarNames(String method) {
+        ArrayList<String> varNames = new ArrayList<>();
+        for(Variable var: variables.get(method)) {
+            varNames.add(var.getName());
+        }
+        return varNames;
+    }
+
+    public static Variable findVariable(String method, String variableName) {
+        ArrayList<Variable> vars = variables.get(method);
+        for(Variable var: vars) {
+            if(var.getName().equals(variableName)) {
+                return var;
+            }
+        }
+        return null;
     }
 }
