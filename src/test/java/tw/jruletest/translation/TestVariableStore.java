d96af655ee502df396ded3c5d455ede43448a457
twride17 <toby@thewrides.com>
2023-08-13 14:14:48 +0100

    Updated Variable store tests

--- src/test/java/tw/jruletest/translation/TestVariableStore.java
+++ src/test/java/tw/jruletest/translation/TestVariableStore.java
@@ -6,30 +6,30 @@ public class TestVariableStore {
 
     @Before
     public void setup() {
-        VariableStore.addVariable("test", "variable");
+        VariableStore.addVariable("test", "variable", int.class);
     }
 
     @Test
     public void testAddVariableToExistingMethod() {
-        VariableStore.addVariable("test", "otherVariable");
-        Assert.assertTrue(VariableStore.getVars("test").contains("otherVariable"));
+        VariableStore.addVariable("test", "otherVariable", int.class);
+        Assert.assertTrue(VariableStore.getVarNames("test").contains("otherVariable"));
     }
 
     @Test
     public void testAddVariableToNewMethod() {
-        VariableStore.addVariable("test1", "variable");
-        Assert.assertEquals(1, VariableStore.getVars("test1").size());
-        Assert.assertTrue(VariableStore.getVars("test1").contains("variable"));
+        VariableStore.addVariable("test1", "variable", int.class);
+        Assert.assertEquals(1, VariableStore.getVarNames("test1").size());
+        Assert.assertTrue(VariableStore.getVarNames("test1").contains("variable"));
     }
 
     @Test
     public void testGetVariableNameWithNoDuplicates() {
-        Assert.assertEquals("variable", VariableStore.getNextUnusedVariableName("newTest", "variable"));
+        Assert.assertEquals("variable", VariableStore.getNextUnusedVariableName("newTest", "variable", int.class));
     }
 
     @Test
     public void testGetVariableNameWithDuplicates() {
-        Assert.assertEquals("variable2", VariableStore.getNextUnusedVariableName("test", "variable"));
+        Assert.assertEquals("variable2", VariableStore.getNextUnusedVariableName("test", "variable", int.class));
     }
 
     @After
