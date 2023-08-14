795e08b48af2e5f349387e8f78c79509fa095f47
twride17 <toby@thewrides.com>
2023-08-14 15:33:28 +0100

    Test Executor no longer tries to load main source files as not necessary for test execution

--- src/main/java/tw/jruletest/TestExecutor.java
+++ src/main/java/tw/jruletest/TestExecutor.java
@@ -29,15 +29,6 @@ public class TestExecutor {
 
         Runner.runCommand(command);
 
-        List<String> classNames = FileFinder.getClassNames(FileFinder.getFiles(System.getProperty("user.dir") + "\\src\\main\\java"), "src\\main\\java\\");
-        for(String className: classNames) {
-            try {
-                Runner.getLoader().loadClass(className);
-            } catch (ClassNotFoundException e) {
-                System.out.println("Could not find " + className);
-            } catch (LinkageError e) {}
-        }
-
         Runner.getLoader().setTopPackage("generated");
         List<File> generatedTestFiles = FileFinder.getFiles("generated");
         for(File generatedFile: generatedTestFiles) {
