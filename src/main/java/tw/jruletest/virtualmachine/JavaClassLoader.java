1b72da5f2ec77563d674c8fd8ff6ad05f2661b7b
twride17 <toby@thewrides.com>
2023-08-15 17:05:01 +0100

    JavaCLassLoader and JavaClassCompiler updated to add overloaded methods for loading classes and java class compilation. Refecrence to loader object moved into static loader class

--- src/main/java/tw/jruletest/virtualmachine/JavaClassLoader.java
+++ src/main/java/tw/jruletest/virtualmachine/JavaClassLoader.java
@@ -1,22 +1,29 @@
 package tw.jruletest.virtualmachine;
 
+import tw.jruletest.Runner;
+import tw.jruletest.analyzers.JavaClassAnalyzer;
+import tw.jruletest.files.FileFinder;
+import tw.jruletest.files.source.SourceClass;
+
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class JavaClassLoader extends ClassLoader {
 
-    private String sourceDirectory = "main";
-    private String topPackage;
+    private static JavaClassLoader loader;
+
+    private String currentDirectory = "\\main\\java\\";
+    private String rootPackage = "";
     private String filePath;
 
     public JavaClassLoader(ClassLoader parent) {
         super(parent);
     }
 
-    public void setTopPackage(String topPackage) {
-        this.topPackage = topPackage;
+    private void setRootPackage(String rootPackage) {
+        this.rootPackage = rootPackage;
     }
 
     public void setFilePath(String path) {
@@ -24,7 +31,7 @@ public class JavaClassLoader extends ClassLoader {
     }
 
     private Class<?> getClass(String name) throws ClassNotFoundException {
-        setFilePath(System.getProperty("user.dir") + "\\src\\" + sourceDirectory + "\\java\\" + name.replaceAll("\\.", "\\\\") + ".class");
+        setFilePath(Runner.getRootPath() + currentDirectory + name.replaceAll("\\.", "\\\\") + ".class");
         try {
             // This loads the byte code data from the file
             byte[] b = loadClassFileData();
@@ -40,7 +47,7 @@ public class JavaClassLoader extends ClassLoader {
 
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
-        if (name.startsWith(topPackage)) {
+        if (name.startsWith(rootPackage)) {
             return getClass(name);
         }
         return super.loadClass(name);
@@ -56,11 +63,57 @@ public class JavaClassLoader extends ClassLoader {
         return buff;
     }
 
-    public void changeDirectory() {
-        if(sourceDirectory.equals("main")) {
-            sourceDirectory = "test";
-        } else {
-            sourceDirectory = "main";
+    public void changeDirectory(String directory) {
+        currentDirectory = directory;
+    }
+
+    // Static methods
+    public static void createLoader() {
+        loader = new JavaClassLoader(Runner.class.getClassLoader());
+    }
+
+    public static void setLoaderRootPackage(String packageName) {
+        loader.setRootPackage(packageName);
+    }
+
+    public static void changeLoaderDirectory(String directory) {
+        loader.changeDirectory(directory);
+    }
+
+    public static JavaClassLoader getLoader() {
+        return loader;
+    }
+
+    public static void loadSourceClasses() {
+        changeLoaderDirectory("\\main\\java\\");
+        loadClasses();
+    }
+
+    public static void loadTestClasses() {
+        changeLoaderDirectory("\\test\\java\\");
+        loadClasses();
+    }
+
+    public static void loadGeneratedTestClasses() {
+        changeLoaderDirectory("\\test\\java\\generated\\");
+        loadClasses();
+    }
+
+    public static void loadClasses(List<String> classes) {
+        JavaClassCompiler.compileClasses("src" + loader.currentDirectory);
+        for(String className: classes) {
+            try {
+                loader.loadClass(className);
+                JavaClassAnalyzer.addSourceClass(new SourceClass(className));
+            } catch (ClassNotFoundException e) {
+                System.out.println("Could not find " + className);
+            } catch (LinkageError e) {
+                System.out.println("Linkage error detected for: " + className);
+            }
         }
+    }
+
+    public static void loadClasses() {
+        loadClasses(FileFinder.getClassNames(FileFinder.getFiles(Runner.getRootPath() + loader.currentDirectory), loader.currentDirectory));
     }
 }
